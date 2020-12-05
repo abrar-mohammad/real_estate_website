576e2cca9bd3fdd96d939c2b59d0245a0250b103
abrar <abrar.stonesour@gmail.com>
2020-12-06 01:52:30 +0600

    2nd commit

--- btre/urls.py
+++ btre/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # whatever it comes, take it to the distributor page: pages.urls(.py)
+    path('', include('pages.urls')),
 ]
